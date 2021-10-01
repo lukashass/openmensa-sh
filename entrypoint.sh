d66dc3b50f5fc028bbb17abc4311065d759c3e62
Lukas Hass <lukas@slucky.de>
2021-10-01 15:40:24 +0200

    create feeds directory on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ mkdir -p /app/http-root/canteens
 cp -r /app/canteen-templates/. /app/http-root/canteens
 find /app/http-root/canteens -type f -exec sed -i -e "s/\[HOST_PLACEHOLDER\]/${HOST}/g" {} \;
 
+mkdir -p /app/http-root/feeds
+
 exec busybox crond -f -l 7 -L /dev/stdout
